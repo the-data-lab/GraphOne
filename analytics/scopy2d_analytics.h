c52473523fbd76b6ee97366cc6c9ddd2ce8ae94a
pradeep-k <pradeepk@gwmail.gwu.edu>
2019-07-17 15:23:34 -0400

    added local level status for bfs 2d

--- analytics/scopy2d_analytics.h
+++ analytics/scopy2d_analytics.h
@@ -3,7 +3,7 @@
 #include "communicator.h"
 
 template<class T>
-void do_stream_bfs(gview_t<T>* viewh, uint8_t* status, Bitmap* bitmap2)
+void do_stream_bfs(gview_t<T>* viewh, uint8_t* lstatus, uint8_t* rstatus, Bitmap* bitmap2)
 {
     index_t  frontier = 0;
     scopy2d_client_t<T>* snaph = dynamic_cast<scopy2d_client_t<T>*>(viewh);
@@ -31,8 +31,8 @@ void do_stream_bfs(gview_t<T>* viewh, uint8_t* status, Bitmap* bitmap2)
         //#pragma omp for nowait
         for (vid_t v = 0; v < snaph->v_count; v++) 
         {
-            vid = v + snaph->v_offset;
-            if(false == bitmap1->get_bit(v) || status[vid] == 255) {
+            vid = v;// + snaph->v_offset;
+            if(false == bitmap1->get_bit(v) || lstatus[vid] == 255) {
                 bitmap1->reset_bit(v);
                 continue;
             }
@@ -45,14 +45,14 @@ void do_stream_bfs(gview_t<T>* viewh, uint8_t* status, Bitmap* bitmap2)
                 local_adjlist = (T*)malloc(prior_sz*sizeof(T));
             }
 
-            level = status[vid];
+            level = lstatus[vid];
             snaph->get_nebrs_out(v, local_adjlist);
 
             for (degree_t i = 0; i < nebr_count; ++i) {
-                sid = get_nebr(local_adjlist, i);
-                if (status[sid] > level + 1) {
-                    status[sid] = level + 1;
-                    bitmap2->set_bit(sid - snaph->dst_offset);//dest
+                sid = get_nebr(local_adjlist, i) -  snaph->dst_offset;
+                if (rstatus[sid] > level + 1) {
+                    rstatus[sid] = level + 1;
+                    bitmap2->set_bit(sid);//dest
                     ++frontier;
                     //if (level == 0) { cout << "," << sid; }
                 }
@@ -64,29 +64,37 @@ void do_stream_bfs(gview_t<T>* viewh, uint8_t* status, Bitmap* bitmap2)
         if (col_rank == col_root) {
             MPI_Reduce(MPI_IN_PLACE, bitmap2->get_start(), bitmap2->get_size(),
                       MPI_UINT64_T, MPI_BOR, col_root, _col_comm);
+            MPI_Reduce(MPI_IN_PLACE, rstatus, snaph->v_count,
+                      MPI_UINT8_T, MPI_MIN, col_root, _col_comm);
         } else {
             MPI_Reduce(bitmap2->get_start(), bitmap2->get_start(), bitmap2->get_size(),
                       MPI_UINT64_T, MPI_BOR, col_root, _col_comm);
+            MPI_Reduce(rstatus, rstatus, snaph->v_count,
+                      MPI_UINT8_T, MPI_MIN, col_root, _col_comm);
         }
 
         MPI_Bcast(bitmap2->get_start(), bitmap2->get_size(), MPI_UINT64_T, row_root, _row_comm);
+        MPI_Bcast(lstatus, snaph->v_count, MPI_UINT8_T, row_root, _row_comm);
         
         MPI_Allreduce(MPI_IN_PLACE, &frontier, 1, MPI_UINT64_T, MPI_SUM, _analytics_comm);
-        MPI_Allreduce(MPI_IN_PLACE, status, v_count, MPI_UINT8_T, MPI_MIN, _analytics_comm);
         //Swap the bitmaps
         bitmap1->swap(bitmap2);
     } while (frontier);
 }
 
-void print_bfs_summary(uint8_t* status, vid_t v_count)
+template<class T>
+void print_bfs_summary(gview_t<T>* viewh, uint8_t* status, vid_t v_count)
 {
-    if (_rank == 1) { 
-        for (int l = 0; l < 10; ++l) {
-            vid_t vid_count = 0;
-            #pragma omp parallel for reduction (+:vid_count) 
-            for (vid_t v = 0; v < v_count; ++v) {
-                if (status[v] == l) ++vid_count;
-            }
+    scopy2d_client_t<T>* snaph = dynamic_cast<scopy2d_client_t<T>*>(viewh);
+    for (int l = 0; l < 10; ++l) {
+        uint64_t vid_count = 0;
+        //#pragma omp parallel for reduction (+:vid_count) 
+        for (vid_t v = 0; v < snaph->v_count; ++v) {
+            if (status[v] == l) ++vid_count;
+        }
+        MPI_Allreduce(MPI_IN_PLACE, &vid_count, 1, MPI_UINT64_T, MPI_SUM, _col_comm);
+            
+        if (_rank == 1) { 
             cout << " Level = " << l << " count = " << vid_count << endl;
         }
     }
@@ -102,13 +110,31 @@ void scopy2d_serial_bfs(gview_t<T>* viewh)
 
     scopy2d_client_t<T>* snaph = sstreamh;
     
-    uint8_t* status = (uint8_t*)malloc(v_count*sizeof(uint8_t));
-    memset(status, 255, v_count);
-
+    int row_rank, col_rank;
+    MPI_Comm_rank(_row_comm, &row_rank);
+    MPI_Comm_rank(_col_comm, &col_rank);
+    int col_root = row_rank;
+    int row_root = col_rank;
+    
     sid_t    root   = 1;
-    status[root] = 0;
     Bitmap bitmap(snaph->v_count);
     int update_count = 0;
+    
+    uint8_t* lstatus = (uint8_t*)malloc(snaph->v_count*sizeof(uint8_t));
+    memset(lstatus, 255, snaph->v_count);
+    uint8_t* rstatus = lstatus;
+    
+    if (root >= snaph->v_offset && root < snaph->v_offset + snaph->v_count) {
+        lstatus[root - snaph->v_offset] = 0;
+    }
+    
+    if (row_rank != col_rank) {
+        rstatus = (uint8_t*)malloc(snaph->v_count*sizeof(uint8_t));
+        memset(rstatus, 255, snaph->v_count);
+        if (root >= snaph->dst_offset && root < snaph->dst_offset + snaph->v_count) {
+            rstatus[root - snaph->dst_offset] = 0;
+        }
+    }
     
     double start = mywtime();
     double end = 0;
@@ -120,12 +146,12 @@ void scopy2d_serial_bfs(gview_t<T>* viewh)
         }
         ++update_count;
         //cout << " Rank " << _rank << " "<< snaph->v_offset << ":" << snaph->v_count << endl;
-        start = mywtime();
-	    do_stream_bfs(snaph, status, &bitmap);
+        //start = mywtime();
+	    do_stream_bfs(snaph, lstatus, rstatus, &bitmap);
         //end = mywtime();
         //cout << " BFS Time at Batch " << update_count << " = " << end - start << endl;
     } 
-    print_bfs_summary(status, v_count); 
+    print_bfs_summary(snaph, lstatus, v_count); 
 }
 
 template<class T>
